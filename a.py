3bee2ce0625bdaa501fc9bf7d025431133d5ec5e
Yuxian <yuxianzhou1113@gmail.com>
2023-05-28 16:21:23 +0800

    modified:   train customized agent 	modified:   complete all functions required

--- a.py
+++ a.py
@@ -8,7 +8,7 @@ import rlcard
 from rlcard.agents.my_agent import MyAgent as MyAgent
 from rlcard.agents import RandomAgent
 from rlcard.utils import set_seed
-from rlcard.utils import print_card
+from rlcard.utils import print_card, reorganize
 
 env = rlcard.make(
         'no-limit-holdem',
@@ -17,6 +17,7 @@ env = rlcard.make(
             'game_players_num':3
         }
 )
+print(env.num_players)
 
 # Seed numpy, torch, random
 set_seed(42)
@@ -30,34 +31,37 @@ random_agent = RandomAgent(num_actions=env.num_actions)
 random_agent2 = RandomAgent(num_actions=env.num_actions)
 env.set_agents([my_agent, random_agent, random_agent2])
 
-for episode in range(30):
-    print(">> Start a new game")
+for episode in range(300):
+	print(">> Start a new game")
 
-    trajectories, payoffs = env.run(is_training=False)
-    # If the human does not take the final action, we need to
-    # print other players action
-    final_state = trajectories[0][-1]
-    action_record = final_state['action_record']
-    state = final_state['raw_obs']
-    _action_list = []
-    for i in range(1, len(action_record)+1):
-        if action_record[-i][0] == state['current_player']:
-            break
-        _action_list.insert(0, action_record[-i])
-    for pair in _action_list:
-        print('>> Player', pair[0], 'chooses', pair[1])
+	trajectories, payoffs = env.run(is_training=False)
+	tr =reorganize(trajectories, payoffs)
+	for ts in tr[0]:
+		state, action, reward, next_state, done = tuple(ts)
+		my_agent.feed_buffer(state['obs'], action, reward, next_state['obs'], next_state['legal_actions'], done)
+	final_state = trajectories[0][-1]
+	action_record = final_state['action_record']
+	state = final_state['raw_obs']
+	_action_list = []
+	for i in range(1, len(action_record)+1):
+		if action_record[-i][0] == state['current_player']:
+			break
+		_action_list.insert(0, action_record[-i])
+	for pair in _action_list:
+		print('>> Player', pair[0], 'chooses', pair[1])
 
-    # Let's take a look at what the agent card is
-    print('===============     Cards all Players    ===============')
-    for hands in env.get_perfect_information()['hand_cards']:
-        print_card(hands)
+# Let's take a look at what the agent card is
+	print('===============     Cards all Players    ===============')
+	for hands in env.get_perfect_information()['hand_cards']:
+		print_card(hands)
 
-    print('===============     Result     ===============')
-    if payoffs[0] > 0:
-        print('You win {} chips!'.format(payoffs[0]))
-    elif payoffs[0] == 0:
-        print('It is a tie.')
-    else:
-        print('You lose {} chips!'.format(-payoffs[0]))
-    print('')
+	print('===============     Result     ===============')
+	if payoffs[0] > 0:
+		print('You win {} chips!'.format(payoffs[0]))
+	elif payoffs[0] == 0:
+		print('It is a tie.')
+	else:
+		print('You lose {} chips!'.format(-payoffs[0]))
+	print('')
+	print(payoffs)
 
--- rlcard/agents/my_agent.py
+++ rlcard/agents/my_agent.py
@@ -12,81 +12,118 @@ from itertools import count
 Transition = namedtuple("Transition", ('state', 'action', 'reward', 'next_state', 'legal_actions', 'done'))
 
 class ReplayBuffer(object):
-    def __init__(self, capacity):
-        self.buffer = deque([], maxlen = capacity)
+	def __init__(self, capacity):
+		self.buffer = deque([], maxlen = capacity)
 
-    def push(self, *args):
-        self.buffer.append(Transition(*args))
+	def push(self, *args):
+		self.buffer.append(Transition(*args))
 
-    def sample(self, batch_size):
-        return random.sample(self.buffer, batch_size)
+	def sample(self, batch_size):
+		return random.sample(self.buffer, batch_size)
 
-    def __len__(self):
-        return len(self.buffer)
+	def __len__(self):
+		return len(self.buffer)
 
 class DQN(nn.Module):
-    def __init__(self, n_observations, n_actions, n_hidden):
-        super(DQN, self).__init__()
-        self.net = nn.Sequential(
-                nn.Linear(n_observations, n_hidden),
-                nn.ReLU(),
-                nn.Linear(n_hidden, n_hidden),
-                nn.ReLU(),
-                nn.Linear(n_hidden, n_actions)
-                )
+	def __init__(self, n_observations, n_actions, n_hidden):
+		super(DQN, self).__init__()
+		self.net = nn.Sequential(
+			nn.Linear(n_observations, n_hidden),
+			nn.ReLU(),
+			nn.Linear(n_hidden, n_hidden),
+			nn.ReLU(),
+			nn.Linear(n_hidden, n_actions)
+			)
 
-    def forward(self, x):
-        return self.net(x.float())
+	def forward(self, x):
+		return self.net(x.float())
 
 
 class MyAgent(object):
-    def __init__(self,
-            num_actions,
-            num_obs,
-            capacity=20000,
-            num_hidden=128,
-            LR = 1e-4,
-            EPS_START=0.9,
-            EPS_END=0.05,
-            EPS_DECAY=20000,
-            device=None
-            ):
-        self.use_raw = False
-        self.num_actions = num_actions
-        self.replay_buffer = ReplayBuffer(capacity)
-        if device==None:
-            self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.policy_net = DQN(num_obs, num_actions, num_hidden).to(self.device)
-        self.target_net = DQN(num_obs, num_actions, num_hidden).to(self.device)
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr = LR, amsgrad=True)
-        self.EPS_START = EPS_START
-        self.EPS_END = EPS_END
-        self.EPS_DECAY = EPS_DECAY
-        self.steps_done=0
+	def __init__(self,
+			num_actions,
+			num_obs,
+			capacity=20000,
+			batch_size=128,
+			num_hidden=128,
+			train_period=100,
+			target_update_period=200,
+			GAMMA = 0.99,
+			LR = 1e-4,
+			EPS_START=0.9,
+			EPS_END=0.05,
+			EPS_DECAY=20000,
+			device=None
+			):
+		self.use_raw = False
+		self.num_actions = num_actions
+		self.replay_buffer = ReplayBuffer(capacity)
+		self.batch_size=batch_size
+		self.train_period = train_period
+		self.target_update_period = target_update_period
+		if device==None:
+			self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+		self.gamma = GAMMA
+		self.policy_net = DQN(num_obs, num_actions, num_hidden).to(self.device)
+		self.target_net = DQN(num_obs, num_actions, num_hidden).to(self.device)
+		self.target_net.load_state_dict(self.policy_net.state_dict())
+		self.optimizer = optim.AdamW(self.policy_net.parameters(), lr = LR, amsgrad=True)
+		self.EPS_START = EPS_START
+		self.EPS_END = EPS_END
+		self.EPS_DECAY = EPS_DECAY
+		self.steps_done=0
+		self.train_done=0
+	
+	def predict(self, state):
+		obs = torch.tensor(state['obs'], dtype=torch.float32, device=self.device).unsqueeze(0)
+		return self.policy_net(obs).view(-1)
 
-    # def step(self,state):
-        # return np.random.choice(list(state['legal_actions'].keys()))
 
-    def eval_step(self, state):
-        probs = [0 for _ in range(self.num_actions)]
-        for i in state['legal_actions']:
-            probs[i] = 1/len(state['legal_actions'])
+	def eval_step(self, state):
+		probs = [0 for _ in range(self.num_actions)]
+		q_values = self.predict(state)
+		for i in state['legal_actions']:
+			probs[i] = q_values[i]
+		info = {}
+		info['probs'] = {state['raw_legal_actions'][i]: probs[list(state['legal_actions'].keys())[i]] for i in range(len(state['legal_actions']))}
+		return self.step(state), info
 
-        info = {}
-        info['probs'] = {state['raw_legal_actions'][i]: probs[list(state['legal_actions'].keys())[i]] for i in range(len(state['legal_actions']))}
+# predict q values of current state and determine the best action 
+	def step(self, state):
+		sample = random.random()
+		eps = self.EPS_END+(self.EPS_START-self.EPS_END)*math.exp(-1*self.steps_done/self.EPS_DECAY)
+		if sample>eps:
+			q_values = self.predict(state)
+			_, action=q_values.max(0)
+			action = int(action.item())
+			return action
+		else:
+			return np.random.choice(list(state['legal_actions'].keys())) 
+	
+	def feed_buffer(self, state, action, reward, next_state, legal_actions, done):
+		self.steps_done+=1
+		self.replay_buffer.push(state, action, reward, next_state, legal_actions, done)
+		if self.steps_done>self.batch_size and self.steps_done%self.train_period==0:
+			self.train()
 
-        return self.step(state), info
+	def train(self):
+		self.train_done+=1
+		transitions = self.replay_buffer.sample(self.batch_size)
+		batch = Transition(*zip(*transitions))
+		state_batch=torch.tensor(batch.state, device=self.device)
+		action_batch=torch.tensor(batch.action, device=self.device)
+		reward_batch=torch.tensor(batch.reward, device=self.device)
+		# computes the q_values of all actions under the state and then choose the one corresponds to the actions selected
+		Vt = self.policy_net(state_batch).gather(1, torch.tensor(batch.action, device=self.device).unsqueeze(1)).view(-1) 
+		Vt_next = self.target_net(state_batch).max(1)[0] 
+		E_Vt = (Vt_next*self.gamma)+reward_batch
 
-    def step(self, state):
-        sample = random.random()
-        eps = self.EPS_END+(self.EPS_START-self.EPS_END)*math.exp(-1*self.steps_done/self.EPS_DECAY)
-        self.steps_done+=1
-        if sample>eps:
-            obs = torch.tensor(state['obs'], dtype=torch.float32, device=self.device).unsqueeze(0)
-            q_values = self.policy_net(obs)
-            _, action=q_values.max(1)
-            action = int(action.item())
-            return action
-        else:
-            return np.random.choice(list(state['legal_actions'].keys())) 
+		criterion = nn.SmoothL1Loss()
+		loss = criterion(Vt, E_Vt.unsqueeze(1))
+		self.optimizer.zero_grad()
+		loss.backward()
+		torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+		self.optimizer.step()
+		if self.train_done>0 and self.train_done%self.target_update_period==0:
+			self.target_net.load_state_dict(self.policy_net.state_dict())
+
